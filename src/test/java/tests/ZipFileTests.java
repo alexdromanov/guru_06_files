c6146090696f87c35662113db0be96e012df25ff
AD <dentist@rambler.ru>
2021-04-14 22:26:59 +0300

    added unzip

--- src/test/java/tests/ZipFileTests.java
+++ src/test/java/tests/ZipFileTests.java
@@ -8,26 +8,25 @@ import java.util.zip.ZipException;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.openqa.selenium.io.Zip.unzip;
+
 import static utils.Files.getFile;
 import static utils.Files.readTextFromPath;
+import static utils.Zip.unzip;
 
 public class ZipFileTests {
     @Test
-    void zipWithPasswordTest() throws IOException, ZipException {
+    void zipWithPasswordTest() throws IOException, ZipException, net.lingala.zip4j.exception.ZipException {
         String zipFilePath = "./src/test/resources/files/11.zip";
         String unzipFolderPath = "./src/test/resources/files/unzip";
         String zipPassword = "";
         String unzipTxtFilePath = "./src/test/resources/files/unzip/1.txt";
         String expectedData = "Ahoy";
 
+
         unzip(zipFilePath, unzipFolderPath, zipPassword);
 
         String actualData = readTextFromPath(unzipTxtFilePath);
 
         assertThat(actualData, containsString(expectedData));
-    }
-
-  //  private void unzip(String zipFilePath, String unzipFolderPath, String zipPassword) {
     }
 }
--- src/test/java/utils/Zip.java
+++ src/test/java/utils/Zip.java
@@ -3,13 +3,16 @@ package utils;
 import net.lingala.zip4j.core.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
 
-public class ZipFiles {
-
-    public static void unZip(String path, String unzippath, String pas) throws ZipException {
-        ZipFile zipFile = new ZipFile((path));
+public class Zip {
+    public static void unzip(String path, String unzipPath, String password) throws ZipException {
+        ZipFile zipFile = new ZipFile(path);
         if (zipFile.isEncrypted()) {
-            zipFile.setPassword(pas);
+            zipFile.setPassword(password);
         }
-        zipFile.extractAll(unzippath);
+        zipFile.extractAll(unzipPath);
+    }
+
+    public static void unzip(String path, String unzipPath) throws ZipException {
+        unzip(path, unzipPath, "");
     }
 }
